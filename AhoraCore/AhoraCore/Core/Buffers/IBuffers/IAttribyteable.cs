53367ab5f780b003777740b39c24d577a7ca8ce3
YuryStrelkov <ghost_strelkov@mail.ru>
2020-08-10 17:24:23 +0400

    +

--- AhoraCore/AhoraCore/Core/Buffers/IBuffers/IAttribyteable.cs
+++ AhoraCore/AhoraCore/Core/Buffers/IBuffers/IAttribyteable.cs
@@ -2,15 +2,15 @@
 {
         public static class VericesAttribytes
         {
-        public static int V_POSITION = 0;
-        public static int V_UVS = 1;
-        public static int V_NORMAL = 2;
-        public static int V_TANGENT = 4;
-        public static int V_BITANGENT = 8;
-        public static int V_BONES = 16;
-        public static int V_BONES_WEIGHTS = 32;
-        public static int V_COLOR_RGB = 64;
-        public static int V_COLOR_RGBA = 128;
+			public static int V_POSITION = 0;
+			public static int V_UVS = 1;
+			public static int V_NORMAL = 2;
+			public static int V_TANGENT = 4;
+			public static int V_BITANGENT = 8;
+			public static int V_BONES = 16;
+			public static int V_BONES_WEIGHTS = 32;
+			public static int V_COLOR_RGB = 64;
+			public static int V_COLOR_RGBA = 128;
         }
 
         public interface IAttribyteable
